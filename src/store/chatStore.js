01af6e23ab228f80e34a9a3f3c6345a7c3bba1aa
Xasanboy <xasanboymatvapayev@icloud.com>
2026-06-09 04:54:29 +0500

    Update chatStore.js

--- src/store/chatStore.js
+++ src/store/chatStore.js
@@ -3,6 +3,8 @@ import { create } from 'zustand'
 export const useChatStore = create((set) => ({
   activeChatId: null,
   onlineUsers: [],
+  typingUsers: [],
   setActiveChatId: (activeChatId) => set({ activeChatId }),
   setOnlineUsers: (onlineUsers) => set({ onlineUsers }),
+  setTypingUsers: (typingUsers) => set({ typingUsers }),
 }))
