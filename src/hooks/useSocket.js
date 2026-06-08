e487f50cc36eded56d495203d47574be1fdbc78c
Xasanboy <xasanboymatvapayev@icloud.com>
2026-06-09 04:50:20 +0500

    Update useSocket.js

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useCallback } from 'react'
 import { getSocket } from '../services/socketClient.js'
 import { useAuthStore } from '../store/authStore.js'
 
@@ -14,4 +14,18 @@ export function useSocket(eventName, handler) {
       socket.off(eventName, handler)
     }
   }, [eventName, handler, token])
+}
+
+// Socket orqali event emit qilish uchun hook
+export function useSocketEmit() {
+  const token = useAuthStore((state) => state.token)
+
+  const emit = useCallback((event, data) => {
+    const socket = getSocket(token)
+    if (socket.connected) {
+      socket.emit(event, data)
+    }
+  }, [token])
+
+  return emit
 }
