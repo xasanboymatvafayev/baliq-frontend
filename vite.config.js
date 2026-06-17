8823030f3365af0659849fef848705600e35346f
Xasanboy <xasanboymatvapayev@icloud.com>
2026-06-17 18:50:44 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3000,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'baliq-savdosi-uz.vercel.app',
+      'localhost',
+    ]
   },
   preview: {
     port: 4173,
