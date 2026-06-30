a05612fb0ff73820ab16543be493991b60b915db
Xasanboy <xasanboymatvapayev@icloud.com>
2026-06-30 15:00:03 +0500

    refactor: useFirebasePhone -> useBackendOtp

---
+++ src/hooks/useFirebasePhone.js
@@ -0,0 +1,3 @@
+// Firebase Phone Auth reCAPTCHA muammosi tufayli o'chirildi.
+// Barcha oqimlar useBackendOtp ga yo'naltirildi (backend OTP + Firebase custom token).
+export { useBackendOtp as useFirebasePhone } from "./useBackendOtp.js";
